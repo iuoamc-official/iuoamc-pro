433e3479ee743ee744149288424e268992b2ff6a
iuoamc-official <info@iuoamc.uk>
2026-09-11 15:46:55 +0100

    Add membership application, issuance, download, and anti routes

--- routes/modules/memberships.php
+++ routes/modules/memberships.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Control\MembershipController;
 use App\Http\Controllers\Control\GovernanceHubController;
+use App\Http\Controllers\MembershipVerificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{locale}/control/governance', [GovernanceHubController::class, 'index'])
@@ -21,7 +22,24 @@ Route::prefix('{locale}/control/memberships')->where(['locale' => 'ar|en|fr'])
             ->middleware('permission:memberships.correct')->name('correct');
         Route::post('/{membership}/correct', [MembershipController::class, 'storeCorrection'])->whereNumber('membership')
             ->middleware(['permission:memberships.correct', 'throttle:10,1'])->name('correction.store');
+        Route::get('/{membership}/application', [MembershipController::class, 'application'])->whereNumber('membership')
+            ->middleware('permission:memberships.manage')->name('application');
+        Route::put('/{membership}/application', [MembershipController::class, 'updateApplication'])->whereNumber('membership')
+            ->middleware(['permission:memberships.manage', 'throttle:20,1'])->name('application.update');
+        Route::post('/{membership}/credentials', [MembershipController::class, 'issueCredentials'])->whereNumber('membership')
+            ->middleware(['permission:memberships.issue', 'throttle:5,1'])->name('credentials.issue');
+        Route::get('/{membership}/credentials/{credential}/{kind}', [MembershipController::class, 'downloadCredential'])
+            ->whereNumber('membership')->whereNumber('credential')->where('kind', 'card|certificate')
+            ->middleware('throttle:20,1')->name('credentials.download');
         Route::post('/{membership}/actions/{action}', [MembershipController::class, 'transition'])
             ->whereNumber('membership')->where('action', 'submit|return|reject|approve|reopen|suspend|reinstate|revoke|renew')
             ->middleware('throttle:60,1')->name('transition');
     });
+
+
+Route::get('/verify/m/{token}', [MembershipVerificationController::class, 'show'])
+    ->where('token', '[a-f0-9]{64}')->middleware('throttle:60,1')->name('memberships.verify');
+Route::get('/verify/m/{token}/proof', [MembershipVerificationController::class, 'proof'])
+    ->where('token', '[a-f0-9]{64}')->middleware('throttle:60,1')->name('memberships.proof');
+Route::get('/verify/m/{invalidToken}', [MembershipVerificationController::class, 'show'])
+    ->where('invalidToken', '[^/]+')->middleware('throttle:60,1')->name('memberships.verify-invalid');

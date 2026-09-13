c4fbf525206346480dc8a29e9b53f1971f8b9f70
iuoamc-official <info@iuoamc.uk>
2026-09-14 00:06:26 +0100

    Add safe Nexus status configuration for IUOAMC TV

--- config/broadcast_tv.php
+++ config/broadcast_tv.php
@@ -5,5 +5,7 @@ return [
     'legacy_url' => env('IUOAMC_TV_LEGACY_URL', 'https://platform-iuoamc.uk/tv'),
     'preview_url' => env('IUOAMC_TV_PREVIEW_URL'),
     'hls_url' => env('IUOAMC_TV_HLS_URL'),
+    'status_url' => env('IUOAMC_TV_STATUS_URL'),
+    'status_timeout' => (float) env('IUOAMC_TV_STATUS_TIMEOUT', 2.5),
     'public_enabled' => env('IUOAMC_TV_PUBLIC_ENABLED', false),
 ];
